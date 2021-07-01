421608df4bb9de146dd343958eedf01ea608d5b2
Sumit V <sumit.v@logmein.com>
2021-07-01 09:36:41 +0530

    basic navigation and static file middleware

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname) + '/index.html');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname) + '/about.html');
+})
+
+app.listen(PORT, () => {
+    console.log('Running');
+});
