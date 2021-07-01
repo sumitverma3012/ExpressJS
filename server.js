0d8a356e4b54d6c271a9f21f64c0805ae44c0b35
Sumit V <sumit.v@logmein.com>
2021-07-01 14:59:57 +0530

    using router module from express

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const path = require('path');
 const express = require('express');
-
+const  mainRouter = require('./routes/index');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(mainRouter)
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname) + '/index.html');
 // })
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'My Home Page'
-    })
-})
+// app.get('/', (req, res) => {
+//     res.render('index', {
+//         title: 'My Home Page'
+//     })
+// })
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'My About Page'
-    })
-})
+// app.get('/about', (req, res) => {
+//     res.render('about', {
+//         title: 'My About Page'
+//     })
+// })
 
 // app.get('/about', (req, res) => {
 //     res.sendFile(path.resolve(__dirname) + '/about.html');
 // })
 
-app.get('/download', (req, res) => {
-    res.download(path.resolve(__dirname) + '/test.txt');
-})
+// app.get('/download', (req, res) => {
+//     res.download(path.resolve(__dirname) + '/test.txt');
+// })
 
 
 app.listen(PORT, () => console.log('Running'));
