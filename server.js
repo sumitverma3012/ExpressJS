ddece4ed48162b8c2d5cb555777ae9c2f92441ab
Sumit V <sumit.v@logmein.com>
2021-07-01 14:41:13 +0530

    ejs templating engine

--- server.js
+++ server.js
@@ -6,14 +6,32 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname) + '/index.html');
+// })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname) + '/index.html');
+    res.render('index', {
+        title: 'My Home Page'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname) + '/about.html');
+    res.render('about', {
+        title: 'My About Page'
+    })
 })
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.resolve(__dirname) + '/about.html');
+// })
+
+app.get('/download', (req, res) => {
+    res.download(path.resolve(__dirname) + '/test.txt');
+})
+
 
 app.listen(PORT, () => {
     console.log('Running');
